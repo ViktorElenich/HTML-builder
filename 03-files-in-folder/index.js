9006efdddcbba5ac49c32f48af70321b6d538d5e
Viktor <velenic@mail.ru>
2022-11-02 13:19:41 +0300

    refactor: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
-const { readdir } = require('fs/promises');
 const path = require('path');
 
 fs.readdir(path.join(__dirname, '/secret-folder'), {withFileTypes: true}, (err, items) => {
-    if(err){
-        console.log(err)
-    } else{
-        for(let item of items){
-            if(item.isDirectory() !== true){
-                getFiles(item)
-            }
-        }
+  if(err){
+    console.log(err);
+  } else{
+    for(let item of items){
+      if(item.isDirectory() !== true){
+        getFiles(item);
+      }
     }
-})
+  }
+});
 async function getFiles(item) {
-    let pathName = path.join(__dirname,`/secret-folder/${item.name}`);
-    let size = 0;
-    size = await fs.promises.stat(pathName).then(stat => {return stat.size});
-    console.log(path.basename(item.name , path.extname(item.name)), '-', path.extname(item.name), '-', size, 'б');
+  let pathName = path.join(__dirname,`/secret-folder/${item.name}`);
+  let size = 0;
+  size = await fs.promises.stat(pathName).then(stat => {return stat.size;});
+  console.log(path.basename(item.name , path.extname(item.name)), '-', path.extname(item.name).split('.').join(''), '-', size, 'б');
 }
+
 
 
 
